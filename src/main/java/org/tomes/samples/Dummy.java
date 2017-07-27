85d53722a39f7089d9c460883cc82d6a1da7d4d7
Tom <schmiddla@gmail.com>
2017-07-27 22:33:46 +0000

    test-class

---
+++ src/main/java/org/tomes/samples/Dummy.java
@@ -0,0 +1,10 @@
+package org.tomes.samples;
+
+import javax.servlet.Filter;
+import org.apache.log4j.Logger;
+
+public class Dummy {
+    Logger log = Logger.getLogger(this.getClass());
+    
+    
+}
